5ad6bd2b9d13fc4a49c0fa207f69076768999550
Javier Colmeiro <javier.colmeiro@impulzia.com>
2020-01-13 08:47:36 +0100

    + missing file

---
+++ models/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+domain = 'magento.com'
+port = 443
+user = ''
+key = ''
+protocol = 'https'
+
+mysqluser = 'user'
+mysqlpassword = 'password'
+mysqlhost = 'host'
+mysqldatabase = 'database'
