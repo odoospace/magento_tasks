4c6f655d0b973558c8b59c2a3ab670865a4ff5e5
Javier Colmeiro <javier.colmeiro@impulzia.com>
2020-01-14 14:57:47 +0100

    + fix inventory

--- models/models.py
+++ models/models.py
@@ -946,9 +946,9 @@ class StockInventory(models.Model):
         @return: True
         """
         result = super(StockInventory, self)._action_done()
-        syncid_obj = self.env.get['syncid.reference']
+        syncid_obj = self.env['syncid.reference']
 
-        for inv in self.browse():
+        for inv in self:
             _logger.info('*** Initiating sync stock inventory adjustment - %s to process...' % len(inv.line_ids))
             con = 1
             m = MagentoAPI(config.domain, config.port, config.user, config.key, proto=config.protocol)
