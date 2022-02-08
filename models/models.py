1b9fa67905e9efe760ae39237d4f9710ca828774
Javier Colmeiro <javier.colmeiro@impulzia.com>
2022-02-08 10:36:13 +0100

    + installable version

--- models/models.py
+++ models/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, exceptions, _
-from odoo.osv.orm import except_orm
 from odoo.exceptions import UserError
 from pprint import pprint
 import json
